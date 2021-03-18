bb8fc364d20d742e0653780c71c58b7288ea3289
mvijfschaft <mvijfschaft@hotmail.com>
2021-03-18 21:36:13 +0100

    Gitconfig: moved user settings to custom config

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,8 @@
 $email = Read-Host -Prompt 'GIT Email'
 $username = Read-Host -Prompt 'GIT username'
 
-git config --global user.email $email
-git config --global user.name $username
+git config --file ./configs/Git/.gitconfig.custom user.email $email
+git config --file ./configs/Git/.gitconfig.custom user.name $username
 
 Remove-Variable email
 Remove-Variable username
