2df00d8aee23c872b50e352d659bb6047d7b888b
Vijfschaft, Marc (ESI) <marc.vijfschaft@adp.com>
2024-04-11 16:43:05 +0200

    Copy formatted guid in clipboard

--- configs/Powershell/modules/misc.psm1
+++ configs/Powershell/modules/misc.psm1
@@ -6,6 +6,8 @@ function .. { Set-Location '..' }
 function la { Get-ChildItem -Force @args }
 function mcd { mkdir @args | Set-Location }
 
+function guid { (New-Guid).ToString("D").ToLower() | Set-Clipboard }
+
 Set-Alias -Name 'l' -Value 'Get-ChildItem'
 Set-Alias -Name 'o' -Value 'Start-Process'
 Set-Alias -Name 'open' -Value 'Start-Process'
